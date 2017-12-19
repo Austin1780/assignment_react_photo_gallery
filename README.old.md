d341cbecdbfbcc2f6d6a9cc23cd3868f1456d2f0
Lakshmi <lakshmi.nishthala@gmail.com>
2017-12-19 13:53:17 -0800

    Switching

--- README.old.md
+++
@@ -1,5 +0,0 @@
-Austin, Lakshmi
-
-# assignment_react_photo_gallery
-
-Finally we can search and sort all our selfies using React
--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import octocat from "./Octocat.png";
 import "./App.css";
-import users from "./users";
+import usersList from "./users";
 import Gallery from "./Gallery";
 import User from "./User";
 
 class App extends Component {
+  constructor() {
+    //to be able to use this
+    super();
+
+    this.state = {
+      login: "",
+      avatar_url: ""
+    };
+  }
+
+  onInputChange = e => {
+    this.setState({
+      login: "",
+      avatar_url: ""
+    });
+  };
+
   render() {
+    //let users = ;
     return (
       <div className="App">
         <header className="App-header">
@@ -14,7 +32,19 @@ class App extends Component {
           <h1 className="App-title">React GitHub User Gallery</h1>
         </header>
         <h4>Search for an individual user:</h4>
-        <User />
+        <div className="usersDisplay container">
+          <div className="row">
+            <form>
+              <Input
+                type="text"
+                name="login"
+                value={name}
+                onInputChange={this.onInputChange}
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
         <h4>Gallery of All Users:</h4>
         <Gallery users={users} />
       </div>
